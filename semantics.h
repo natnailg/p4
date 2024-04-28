09d76d5bfd7ae514a98a5d97a816d4933a76cce7
natnailg <natnailgebru@gmail.com>
2024-04-27 20:03:47 -0500

    trying to get the table array to be zero also from the semantics.

--- semantics.h
+++ semantics.h
@@ -8,9 +8,9 @@
 
 extern const int max_size_table;
 extern char* table_array[];
-int table_index;
-bool t2_found;
-bool  string_check;
+extern int table_index;
+extern bool t2_found;
+extern bool  string_check;
 
 
 void pre_order_traversal(node_t* root, int );
