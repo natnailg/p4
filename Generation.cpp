6d7eefc08555d826127a901fae4a824643ec8110
natnailg <natnailgebru@gmail.com>
2024-04-30 16:28:46 -0500

    got the first production of E working only thing left is production 2 of E. I can't wrap my head how it works might need to do another three for that.

--- Generation.cpp
+++ Generation.cpp
@@ -188,7 +188,7 @@ void funcE(node_t* node){
             //we then load that
             fprintf(global_file_pointer, "LOAD %s\n", value);
             fprintf(global_file_pointer, "Loop: %s\n", vlaue);
-            fprintf(global_file_pointer, "")
+//            fprintf(global_file_pointer, "")
 
 //            // since F give me an int or Identifier we need to load that in
 //            fprintf(global_file_pointer, "LOAD %s\n", value);
