8d0b8d544f47e6e23d6849d45d3aa3314503977b
natnailg <natnailgebru@gmail.com>
2024-04-30 14:53:06 -0500

    it was because i was calling right instead for far right in production one of E when calling H. ,AAH

--- Generation.cpp
+++ Generation.cpp
@@ -167,7 +167,7 @@ void funcE(node_t* node){
 //            value1 0
             fprintf(global_file_pointer, "LOAD %s\n", value1);
             fprintf(global_file_pointer, "SUB %s\n", value2);
-            fprintf(global_file_pointer, "BRZNEG: out1\n");
+            fprintf(global_file_pointer, "BRZNEG out1\n");
             funcH(node->far_right);
             fprintf(global_file_pointer, "out1: NOOP\n");
 
